7e4eeb57cdfe6c5f80115c1fbfda3bcafb96c858
Neander de Souza <neander.faria@hotmail.com>
2024-09-27 11:00:15 -0300

    Toolbar

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,45 @@
-export default function DocumentIdPage() {
+"use client";
+
+import { useQuery } from "convex/react";
+
+import { Toolbar } from "@/components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+
+interface DocumentIdPageProps {
+    params: {
+        documentId: Id<"documents">;
+    };
+};
+
+export default function DocumentIdPage({ params }: DocumentIdPageProps) {
+    const document = useQuery(api.documents.getById, {
+        documentId: params.documentId,
+    });
+
+    if (document === undefined) {
+        return (
+            <div>
+                Loading...
+            </div>
+        );
+    }
+
+    if (document === null) {
+        return (
+            <div>
+                Not found
+            </div>
+        );
+    }
+
     return (
-        <div>Document ID</div>
+        <div className="pb-40">
+            <div className="h-[35vh]"></div>
+
+            <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+                <Toolbar initialData={document} />
+            </div>
+        </div>
     );
 }
